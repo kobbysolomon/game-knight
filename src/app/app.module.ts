24911fea6917a5a507599c2224cf804f50bd5f52
Godfried Solomon <godfried.solomon@apollomce.com>
2023-10-13 22:52:46 -0400

    bootstrap and fonts added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router'; // 1. Import RouterModule and Routes
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+
+const routes: Routes = [ // 2. Define a route for the WelcomeComponent
+  { path: 'welcome', component: WelcomeComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes) // 3. Add RouterModule to imports array and call forRoot() method with Routes array
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
