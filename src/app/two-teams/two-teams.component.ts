d453a689f40337d48758a1cf52eee707de486109
Godfried Solomon <godfried.solomon@apollomce.com>
2023-10-30 02:01:32 -0400

    two team page created, score increment functions created

--- src/app/two-teams/two-teams.component.ts
+++ src/app/two-teams/two-teams.component.ts
@@ -7,7 +7,43 @@ import { Component } from '@angular/core';
 })
 export class TwoTeamsComponent {
 
-  scoreToWin: number = 21;
+
+  scoreToWinRound: number = 21;
+  scoreIncrement: number = 1;
+
   roundsPlayed: number = 0;
+  teamOneRoundsWon: number = 0;
+  teamTwoRoundsWon: number = 0;
+
+
+  teamOneName: string = "Team One";
+  teamOneScore: number = 18;
+
+  teamTwoName: string = "Team Two";
+  teamTwoScore: number = 36;
+
+
+
+  increaseTeamOneScore() {
+    this.teamOneScore += this.scoreIncrement;
+  }
+
+  decreaseTeamOneScore() {
+    this.teamOneScore -= this.scoreIncrement;
+  }
+
+
+  increaseTeamTwoScore() {
+    this.teamTwoScore += this.scoreIncrement;
+  }
+
+  decreaseTeamTwoScore() {
+    this.teamTwoScore -= this.scoreIncrement;
+  }
+
+
+
+  
+
 
 }
